796732fc8f77ef4ae6267c444ea7ecd6a73cf55f
Thierry Nicola <tnicola@weo-water.com>
2025-03-19 11:46:19 +0100

    Deploy to GH

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const base = process.env.BASE_PATH || "/humanitech-side"; // Change to your repo name
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -8,8 +10,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		paths: {
+		  base: base
+		},
+		appDir: "internal"
+	  }
 };
 
 export default config;
